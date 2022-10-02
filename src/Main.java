aa9d4eb82c73c3bc0c000cedbe500bde24d43f1b
Andrey Igumnov <igumnov92@gmail.com>
2022-10-02 10:03:53 +0300

    Внесение поправок(разделение дефисом)

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,13 @@ public class Main {
         people.add(new Person("Эллой", "Хорайзан ван лав", 21));
         people.add(new Person("Артур", "Морган фримен халф", 46));
         people.add(new Person("Дзин", "Сакай", 34));
-        people.add(new Person("Тим", "Кук аппле", 61));
+        people.add(new Person("Тим", "Кук-аппле", 61));
 
         System.out.println(people);
         System.out.println();
         Comparator<Person> comp = (p1, p2) -> {
-            String[] s1 = p1.getSurname().split(" ");
-            String[] s2 = p2.getSurname().split(" ");
+            String[] s1 = p1.getSurname().split("\\P{IsAlphabetic}+");
+            String[] s2 = p2.getSurname().split("\\P{IsAlphabetic}+");
             int maxSurname = 3;
             if (s1.length == s2.length) {
                 return Integer.compare(p2.getAge(), p1.getAge());
